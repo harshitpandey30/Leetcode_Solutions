5a02f47d14c3f7ce7bf9934aa26ae78c4145a5e2
harshitpandey30 <113591869+harshitpandey30@users.noreply.github.com>
2022-11-08 17:11:04 +0530

    Time: 2 ms (84.62%), Space: 41.4 MB (40.39%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        else{
+        int l=1;
+        int r=x;
+        while(l<=r){
+             int mid=(l+r)/2;
+             int temp=x/mid;
+            if(temp==mid){
+                return mid;
+            }
+            else if(temp<mid){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l-1;
+        }
+    }
+}
